2f966d635e072e1d048b4535c40070871885621a
“ttamara96” <takacstamarabeata@gmail.com>
2024-07-10 15:50:14 +0200

    Add HU translation for Material components

--- theme.ts
+++ theme.ts
@@ -1,9 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 import { amber, indigo } from '@mui/material/colors';
+import { huHU } from '@mui/x-data-grid/locales';
+import { huHU as coreHuHU } from '@mui/material/locale';
 
-export const theme = createTheme({
+export const theme = createTheme(
+  {
     palette: {
       primary: indigo,
       secondary: amber,
     },
-  });
+  },
+  huHU, // x-data-grid translations
+  coreHuHU, // core translations
+);
